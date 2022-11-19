e2a8d6f8b0a695a804a6aa9a1c499b18442480dc
anheejeong <100365693+anheejeong@users.noreply.github.com>
2022-11-20 05:09:48 +0900

    Add files via upload

---
+++ batteryData.js
@@ -0,0 +1,23 @@
+var batteryData = [
+    {
+        title : "대구",
+        content : "영진전문대학교 복현캠퍼스",
+        distance : "3km",
+        lat : 35.8956224,
+        lng : 128.6224266,
+    },
+    {
+        title : "대구",
+        content : "엑스코",
+        distance : "4km",
+        lat : 35.9068477,
+        lng : 128.613313,
+    },
+    {
+        title : "대구",
+        content : "침산코오롱하늘채 2차아파트",
+        distance : "5km",
+        lat : 35.8860055,
+        lng : 128.5960643,
+    }
+]
---
+++ liftData.js
@@ -0,0 +1,23 @@
+var liftData = [
+    {
+        title : "대구",
+        content : "신암공원",
+        distance : "3km",
+        lat : 35.8894896,
+        lng : 128.6220811,
+    },
+    {
+        title : "대구",
+        content : "대구공업고등학교",
+        distance : "4km",
+        lat : 35.883137,
+        lng : 128.6138841,
+    },
+    {
+        title : "대구",
+        content : "대구실내체육관",
+        distance : "5km",
+        lat : 35.893461,
+        lng : 128.6034161,
+    }
+]
